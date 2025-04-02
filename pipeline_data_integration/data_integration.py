89641cb04fed9d35438bd3a1a294101117990c0b
Jules Sipp <19290903+Questiero@users.noreply.github.com>
2025-04-02 13:47:26 +0200

    Data augmentation

--- pipeline_data_integration/data_integration.py
+++ pipeline_data_integration/data_integration.py
@@ -1,26 +1,56 @@
 import pandas as pd
+import json
 import ollama
 from pydantic import BaseModel, ValidationError
 from typing import Literal
+import re
+from tqdm import tqdm
 
 '''
 Steps :
-- load des outfits
-- augmentation des tags sur les outfits
-X extraction des habits (pas besoin utiliser les extractions de base)
-- augmentation des tags sur les habits
+X load des outfits
+X augmentation des tags sur les outfits
+- extraction des habits (pas besoin utiliser les extractions de base)
+- augmentation des tags sur les habits (ça va être très simple)
 - création d'une base d'outfit
+- Embedding des habits
 '''
 
-OUTFITS_DATA_AUGMENTATION_PROMPT = ''
-CLOTHES_DATA_AUGMENTATION_PROMPT = ''
+OUTFITS_DATA_AUGMENTATION_PROMPT = 'Evaluate this image, your message NEEDING to follow the following JSON format :\n\
+{\n\
+"caption": [YOUR CAPTION], \n\
+"type": \'Casual\'//\'Formal\'//\'Sports\'//\'Party\'/\'Home\', \n\
+"weatherSuitability": \'Cold\'//\'Neutral\'//\'Warm\', \n\
+}'
+
+CLOTHES_DATA_AUGMENTATION_PROMPT = 'Evaluate this image, your message NEEDING to follow the following JSON format :\n\
+{\n\
+"caption": [YOUR CAPTION], \n\
+"color": \'blue\'//\'cyan\'//\green\'//\black\'//\yellow\'//\red\'//\magenta\'//\white\', \n\
+}'
+
+
 DATA_AUGMENTATION_MODEL = 'llava'
 DATASET_OUTFITS_PATH = 'data/clothing-coparsing-dataset/metadata.csv'
 DATASET_OUTFITS_CLASS_DICT_PATH = 'data/clothing-coparsing-dataset/class_dict.csv'
 
+RETRY_LIMIT = 3
+
+def extract_json(text):
+
+    pattern = r'\{.*?\}'
+
+    match = re.search(pattern, text, re.DOTALL)
+
+    if match:
+        return match.group(0)
+    
+    return None
+
 class OutfitCaption(BaseModel):
     caption: str
     type: Literal['Casual', 'Formal', 'Sports', 'Party', 'Home']
+    weatherSuitability: Literal['Cold', 'Neutral', 'Warm']
 
 class ClothesCaption(BaseModel):
     caption: str
@@ -46,40 +76,68 @@ def call_model(model: str, system_prompt: str, prompt: str, image_path: str):
 
 def load_outfits():
     df_metadata = pd.read_csv(DATASET_OUTFITS_PATH)
-    return df_metadata[df_metadata["label_type"] == 'image-level']
+    return df_metadata[df_metadata["label_type"] == 'image-level'].head(3)
 
 def load_class_dict():
     return pd.read_csv(DATASET_OUTFITS_CLASS_DICT_PATH)
 
 def outfits_data_augmentation(image_path: str):
-    response = call_model(DATA_AUGMENTATION_MODEL, OUTFITS_DATA_AUGMENTATION_PROMPT, "", image_path)
 
-    try:
-        return response.model_validate_json()
-    except ValidationError:
-        print('oskour')
+    for i in range(RETRY_LIMIT):
+
+        response = call_model(DATA_AUGMENTATION_MODEL, OUTFITS_DATA_AUGMENTATION_PROMPT, "", image_path)
+
+        try:
+            return OutfitCaption.model_validate_json(extract_json(response))
+        except ValidationError:
+            print(f"Validation ({i+1}/{RETRY_LIMIT} failed. Retrying...)")
+            pass
+
+    raise ValidationError
 
 def clothes_data_augmentation(image_path: str):
-    response = call_model(DATA_AUGMENTATION_MODEL, CLOTHES_DATA_AUGMENTATION_PROMPT, "", image_path)
 
-    try:
-        return response.model_validate_json()
-    except ValidationError:
-        print('oskour2')
+    for i in range(RETRY_LIMIT):
+
+        response = call_model(DATA_AUGMENTATION_MODEL, CLOTHES_DATA_AUGMENTATION_PROMPT, "", image_path)
+
+        try:
+            return ClothesCaption.model_validate_json(extract_json(response))
+        except ValidationError:
+            print(f"Validation ({i+1}/{RETRY_LIMIT} failed. Retrying...)")
+            pass
+
+    raise ValidationError
 
 def data_integration_pipeline():
+
     outfits = load_outfits()
-    outfits_class_dict = load_class_dict()
+    outfits_class_dict = load_class_dict()  # Make an augmented row too?
+
+    idx_to_drop = []
 
     # dataset outfit augmentation
-    for index, row in outfits.iterrows():   
-        outfit_caption = outfits_data_augmentation(f"data/clothing-coparsing-dataset/{row["image_path"]}")
-        outfits['caption'] = outfit_caption.caption
-        outfits['type'] = outfit_caption.type
+    for idx, row in tqdm(outfits.iterrows()):
 
-    df_clothes_from_outfits = []
+        try:
 
-    for index, row in outfits.iterrows():
+            outfit_caption = outfits_data_augmentation(f'data/clothing-coparsing-dataset/{row["image_path"]}')
+            outfits.at[idx, 'caption'] = outfit_caption.caption
+            outfits.at[idx, 'type'] = outfit_caption.type
+
+            print(f"Succesfully augmented row {idx}")
+
+        except ValidationError:
+            print(f"Validation failed after {RETRY_LIMIT} tries. Skipping row {idx}.")
+            idx_to_drop.append(idx)
+            continue
+
+    outfits = outfits.drop(idx_to_drop)
+
+    # df_clothes_from_outfits = []
+
+    # for index, row in outfits.iterrows():
+    #     pass
         
     # oskour
     # for h in outfits.habits:
@@ -87,5 +145,7 @@ def data_integration_pipeline():
     #     df_clothes_from_outfits.append("", "", "", "")
 
 
-    clothes_data_augmentation()
+    # clothes_data_augmentation()
     return
+
+data_integration_pipeline()
